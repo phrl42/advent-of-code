33fde5aebc4e356d4ce329db6717803700df3ef4
phrl42 <phrl42@riseup.net>
2023-09-17 16:19:26 +0200

    solved day 2 part 1

--- 2018/2/main.c
+++ 2018/2/main.c
@@ -16,16 +16,18 @@ bool array_contains(char *arr, size_t size, char comp)
 
 int main()
 {
-  UString string;
+  UString file_string;
 
-  util_string_from_file(&string, "list.txt");
+  util_string_from_file(&file_string, "list.txt");
 
+
+  UString string = file_string;
   char unique[LENGTH] = {0};
   int unique_index = 0;
   
   char line[LENGTH] = {0};
-
-  for(size_t i = 0; i < util_string_line_count(string); i++)
+  int line_count = util_string_line_count(string);
+  for(size_t i = 0; i < line_count; i++)
   {
     util_string_get_line(string, line, LENGTH);
 
@@ -40,25 +42,49 @@ int main()
     util_string_remove_line(&string);
   }
 
+  int count_two = 0;
+  int count_three = 0;
+
+  string = file_string;
   
-  for(size_t u = 0; u < unique_index+1; u++)
+  printf("%s\n", unique);
+  line_count = util_string_line_count(string);
+  
+  for(size_t i = 0; i <= line_count; i++)
   {
-    for(size_t i = 0; i < util_string_line_count(string); i++)
+    bool two_done = false;
+    bool three_done = false;
+    util_string_get_line(string, line, LENGTH);
+    printf("%s\n", line);
+    for(size_t u = 0; u < unique_index; u++)
     {
       int count = 0;
-      util_string_get_line(string, line, LENGTH);
-
-      for(size_t j = 0; j < util_string_line_char_count(string); j++)
+      int char_count = util_string_line_char_count(string);
+      for(size_t j = 0; j < char_count; j++)
       {
 	if(line[j] == unique[u])
 	{
 	  count += 1;
 	}
       }
-      util_string_remove_line(&string);
+      if(count == 2 && !two_done)
+      {
+	two_done = true;
+	printf("granted two in line %d\n", i);
+	count_two += 1;
+      }
+      if(count == 3 && !three_done)
+      {
+	three_done = true;
+	printf("granted three in line %d\n", i);
+	count_three += 1;
+      }
     }
+    
+    
+    util_string_remove_line(&string);
   }
 
-  printf("%s\n", unique);
+  printf("%d * %d = %d\n", count_two, count_three, count_two * count_three);
   return 0;
 }

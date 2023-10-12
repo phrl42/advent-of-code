b79ccebe555caae84d4d27801fa6d105c037e2dc
phrl42 <phrl42@riseup.net>
2023-10-12 14:31:00 +0200

    working on day 6 visualization

--- 2018/6/main.c
+++ 2018/6/main.c
@@ -2,7 +2,32 @@
 #include "util.h"
 
 #define LENGTH 1500
-#define SIZE 300
+#define SIZE 500
+#define TMP 10
+void get_x(UString string, int *x)
+{
+
+  char number_string[TMP] = {0};
+  int index = 0;
+  while(string.data[0] != ',')
+  {
+    number_string[index] = string.data[0];
+    string.data++;
+    index += 1;
+  }
+  *x = atoi(number_string);
+}
+
+void get_y(UString string, int *y)
+{
+  char number_string[TMP] = {0};
+  int index = 0;
+  while(string.data[0] != ' ')
+  {
+    string.data++;
+  }
+  *y = atoi(string.data); 
+}
 
 int main()
 {
@@ -15,11 +40,17 @@ int main()
 
   util_string_separate_by_line(file_string, string, line_count);
   
+  int x[SIZE] = {0};
+  int y[SIZE] = {0};
+
   for(size_t i = 0; i < line_count; i++)
   {
-    util_string_print(string[i]);
+    get_x(string[i], &x[i]);
+    get_y(string[i], &y[i]);
+    //util_string_print(string[i]);
   }
 
+  
   for(size_t i = 0; i < line_count; i++)
   {
     util_string_free(&string[i]);
